03e6b828c11132f135931b1e5d06ddd584af071d
Namrata Jadhav <109957416+NamrataJadh@users.noreply.github.com>
2025-08-28 14:58:08 +0530

    added fonts and styles and fixed react native version error

--- app/_layout.tsx
+++ app/_layout.tsx
@@ -1,5 +1,24 @@
-import { Stack } from "expo-router";
+import { Stack } from 'expo-router';
+import * as SplashScreen from 'expo-splash-screen';
+import { useEffect, useState } from "react";
 
-export default function RootLayout() {
-  return <Stack />;
+// Keep splash screen visible
+SplashScreen.preventAutoHideAsync();
+
+const RootLayout = () => {
+  const [isLogin, setIsLogin] = useState(false)
+  useEffect(() => {
+    setTimeout(() => {
+      SplashScreen.hideAsync();
+    }, 2000);
+
+  })
+
+  return (
+    <>
+    <Stack screenOptions={{headerShown: false}}/>
+    </>
+  )
 }
+
+export default RootLayout
--- app/index.tsx
+++ app/index.tsx
@@ -1,15 +1,33 @@
-import { Text, View } from "react-native";
+import {
+  Poppins_400Regular as fontReg, Poppins_700Bold as fontBold, useFonts
+} from "@expo-google-fonts/poppins";
+import React from "react";
+import { StyleSheet, Text, View } from "react-native";
 
 export default function Index() {
+  useFonts({
+    fontBold,
+    fontReg,
+  });
+
   return (
-    <View
-      style={{
-        flex: 1,
-        justifyContent: "center",
-        alignItems: "center",
-      }}
-    >
-      <Text>Welcome to Otp Verification Demo!!</Text>
+    <View style={styles.main}>
+      <Text style={styles.bodyTxt}>Welcome to Otp Verification Demo!!</Text>
     </View>
   );
 }
+
+
+const styles = StyleSheet.create({
+  main: {
+    flex: 1,
+    backgroundColor: "#ebe8e8",
+    justifyContent: "center",
+    alignItems: "center",
+  },
+  bodyTxt: {
+    fontSize: 15,
+    fontFamily: "fontBold",
+    color: "#6A334D"
+  },
+})
